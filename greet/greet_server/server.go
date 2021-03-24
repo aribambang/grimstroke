b7796760006e99ce1a034723470da08f067f4b04
Ari Bambang Kurniawan <aribk210@gmail.com>
2021-03-24 11:51:25 +0700

    inital commit

---
+++ greet/greet_server/server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+}
